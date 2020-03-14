295c6761cf04193954a0e1c0d7660f3925ec3840
kkeundotnet <kkeundotnet@gmail.com>
2020-03-14 23:43:10 +0000

    Use kkmarkdown in browser
    YES!!

--- src/js/kkmarkdownJs.ml
+++ src/js/kkmarkdownJs.ml
@@ -2,7 +2,7 @@ open Js_of_ocaml
 module Kkmarkdown = Kkmarkdown_lib.Kkmarkdown
 
 let () =
-  Js.export_all
+  Js.export "kkmarkdown"
     (object%js
        val trans =
          fun s -> Js.string (Kkmarkdown.trans_to_string (Js.to_string s))
