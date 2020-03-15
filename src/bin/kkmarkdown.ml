6a4ef67f9762a0b854d360f4824501b05e125e23
kkeundotnet <kkeundotnet@gmail.com>
2020-03-15 00:11:19 +0000

    Read from markdown file

--- src/bin/kkmarkdown.ml
+++ src/bin/kkmarkdown.ml
@@ -7,16 +7,29 @@ let get_intput_from_stdin () =
       while true do
         rev := read_line () :: !rev
       done
-    with End_of_file -> () ) ;
+    with End_of_file -> () );
   List.rev !rev
 
 let print_output_to_stdout bls =
-  Kkmarkdown.pp F.std_formatter bls ;
+  Kkmarkdown.pp F.std_formatter bls;
   F.print_flush ()
 
 let main () =
-  let input = get_intput_from_stdin () in
-  let output = Kkmarkdown.trans_from_lines input in
-  print_output_to_stdout output
+  if Array.length Sys.argv >= 2 then (
+    let rev_lines = ref [] in
+    let ic = open_in Sys.argv.(1) in
+    try
+      while true do
+        rev_lines := input_line ic :: !rev_lines
+      done
+    with End_of_file ->
+      close_in_noerr ic;
+      Kkmarkdown.trans_from_lines (List.rev !rev_lines)
+      |> Kkmarkdown.pp F.std_formatter;
+      F.print_flush () )
+  else
+    let input = get_intput_from_stdin () in
+    let output = Kkmarkdown.trans_from_lines input in
+    print_output_to_stdout output
 
 let () = main ()
