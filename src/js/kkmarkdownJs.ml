79a8ca8f0f0f339b37013da0933f30eed890f4d1
kkeundotnet <kkeundotnet@gmail.com>
2020-03-20 02:20:55 +0000

    Rename field

--- src/js/kkmarkdownJs.ml
+++ src/js/kkmarkdownJs.ml
@@ -7,7 +7,9 @@ let () =
        val trans =
          fun s -> Js.string (Kkmarkdown.trans_to_string (Js.to_string s))
 
-       val unsafe_trans =
+       (* Note: Do not use underscore in the method name.
+          https://ocsigen.org/js_of_ocaml/2.4.1/manual/library.html *)
+       val unsafe =
          fun s ->
            Js.string (Kkmarkdown.trans_to_string ~unsafe:true (Js.to_string s))
     end)
