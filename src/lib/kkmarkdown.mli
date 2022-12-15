35c40e90dd72be46d125d14c00d873259a248e34
kkeundotnet <kkeundotnet@gmail.com>
2022-12-16 01:11:06 +0900

    Replace kkmarkdown module with kkmarkdown2 module

--- src/lib/kkmarkdown.mli
+++ src/lib/kkmarkdown.mli
@@ -1,7 +1,4 @@
-type t
-
-val pp : ?rss:bool -> Format.formatter -> t -> unit
-val trans : ?unsafe:bool -> string -> Typ.block list
-val trans_from_file : ?unsafe:bool -> string -> Typ.block list
-val trans_from_stdin : ?unsafe:bool -> unit -> Typ.block list
+val trans : ?unsafe:bool -> string -> Typ.t
+val trans_from_file : ?unsafe:bool -> string -> Typ.t
+val trans_from_stdin : ?unsafe:bool -> unit -> Typ.t
 val trans_to_string : ?unsafe:bool -> ?rss:bool -> string -> string
