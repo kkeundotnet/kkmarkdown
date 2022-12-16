a8f0f226c14dfef75d2017ac028294dfe9cb36d9
kkeundotnet <kkeundotnet@gmail.com>
2022-12-16 08:20:04 +0000

    Add comment in kkmarkdown.mli

--- src/lib/kkmarkdown.mli
+++ src/lib/kkmarkdown.mli
@@ -1,4 +1,14 @@
+(** Main entry for translation functions *)
+
 val trans : ?unsafe:bool -> string -> Typ.t
+(** @param unsafe Enable unsafe rules. False by default. *)
+
 val trans_from_file : ?unsafe:bool -> string -> Typ.t
+(** @param unsafe Enable unsafe rules. False by default. *)
+
 val trans_from_stdin : ?unsafe:bool -> unit -> Typ.t
+(** @param unsafe Enable unsafe rules. False by default. *)
+
 val trans_to_string : ?unsafe:bool -> ?rss:bool -> string -> string
+(** @param unsafe Enable unsafe rules. False by default.
+@param rss Suppress the elements that are inappropriate for RSS. False by default. *)
