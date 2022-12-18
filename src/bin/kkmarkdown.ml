809001e925f7eb6f927f4d003bf072ee6a31fb8a
kkeundotnet <kkeundotnet@gmail.com>
2022-12-18 21:44:48 +0900

    Add --version option

--- src/bin/kkmarkdown.ml
+++ src/bin/kkmarkdown.ml
@@ -4,19 +4,30 @@ module Typ = Lib.Typ
 
 type env = { rss : bool; unsafe : bool; input_files : string list }
 
+let show_version () =
+  let version =
+    match Build_info.V1.version () with
+    | None -> "n/a"
+    | Some v -> Build_info.V1.Version.to_string v
+  in
+  print_endline version;
+  exit 0
+
 let parse_arg () =
   let rss = ref false in
   let unsafe = ref false in
   let input_files_rev = ref [] in
   let speclist =
     [
-      ("--rss", Arg.Set rss, "Enable rss mode");
+      ("--rss", Arg.Set rss, "Enable RSS mode");
       ("--unsafe", Arg.Set unsafe, "Enable unsafe mode");
+      ("--version", Arg.Unit show_version, "Show version");
     ]
   in
   Arg.parse speclist
     (fun file -> input_files_rev := file :: !input_files_rev)
     {|Usage: kkmarkdown [OPTION]... [FILE]
+
 If FILE is not given, it reads input from stdin.  Visit https://github.com/kkeundotnet/kkmarkdown
 for more information.
 |};
