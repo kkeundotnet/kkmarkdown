b81609e390b6b7cef20702cd958f7b115e5e8954
kkeundotnet <kkeundotnet@gmail.com>
2023-05-22 23:54:53 +0100

    Format

--- scripts/gen_syntax_doc.sh
+++ scripts/gen_syntax_doc.sh
@@ -23,7 +23,7 @@ for file in $MLIS; do
     awk "/SYNTAX: START/{ syntax=1; }
          /\(\*\* /{ if (syntax) { flag=1; print \"\n{1 $base}\n\"; } }
          / \*\)/{ unset_flag=1; }
-    	 /SYNTAX: END/{ syntax=0; }
+         /SYNTAX: END/{ syntax=0; }
          { ret=flag; if (unset_flag) { flag=0; unset_flag=0; } }
          ret" $file \
     | sed -E -e 's/\(\*\* | \*\)//g' \
