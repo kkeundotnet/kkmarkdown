4d0b37927133b1d6508dccadb602b3ea38c9b2d2
kkeundotnet <kkeundotnet@gmail.com>
2024-07-14 18:39:36 +0100

    Add a comment

--- src/lib/BlockRuleDetails.mli
+++ src/lib/BlockRuleDetails.mli
@@ -1,12 +1,14 @@
 (* SYNTAX: START *)
-(** Details:
+(** Details
 
 {[
 |> Title
 
 contents
 <|
-]} *)
+]}
+
+Note that nested details are not supported. *)
 (* SYNTAX: END *)
 
 include BlockRule.S
