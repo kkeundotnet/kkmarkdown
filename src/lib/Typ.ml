a8f0bef5862c0165997ca88031c1db4bc5905faa
kkeundotnet <kkeundotnet@gmail.com>
2024-07-17 23:35:23 +0100

    Use Format.dprintf for pp
    Format.dprintf makes it more intuitive to compose pretty printers.

--- src/lib/Typ.ml
+++ src/lib/Typ.ml
@@ -34,118 +34,107 @@ type block =
 and li = Li of span list | LiP of t
 and t = block list
 
-let pp_list ?(pp_sep = fun _f -> ()) pp f l =
-  let rec aux = function
-    | [] -> ()
-    | [ x ] -> pp f x
-    | hd :: tl ->
-        pp f hd;
-        pp_sep f;
-        aux tl
+let pp_nop _f = ()
+let pp_print_char c f = F.pp_print_char f c
+let pp_print_string s f = F.pp_print_string f s
+
+let ( ++ ) pp1 pp2 f =
+  pp1 f;
+  pp2 f
+
+let pp_list ?(pp_sep = pp_nop) pp l =
+  let rec aux acc = function
+    | [] -> acc
+    | [ x ] -> acc ++ pp x
+    | hd :: tl -> aux (acc ++ pp hd ++ pp_sep) tl
   in
-  aux l
+  aux pp_nop l
 
-let pp_char f = function
-  | '&' -> F.pp_print_string f "&amp;"
-  | '<' -> F.pp_print_string f "&lt;"
-  | '>' -> F.pp_print_string f "&gt;"
-  | '"' -> F.pp_print_string f "&quot;"
-  | '\'' -> F.pp_print_string f "&apos;"
-  | c -> F.pp_print_char f c
+let pp_char = function
+  | '&' -> pp_print_string "&amp;"
+  | '<' -> pp_print_string "&lt;"
+  | '>' -> pp_print_string "&gt;"
+  | '"' -> pp_print_string "&quot;"
+  | '\'' -> pp_print_string "&apos;"
+  | c -> pp_print_char c
 
-let pp_chars f s = String.iter (pp_char f) s
+let pp_chars s f = String.iter (fun c -> pp_char c f) s
 
-let pp_classes f classes =
-  pp_list ~pp_sep:(fun f -> F.pp_print_char f ' ') F.pp_print_string f classes
+let pp_classes classes =
+  pp_list ~pp_sep:(pp_print_char ' ') pp_print_string classes
 
-let pp_open ?classes f tag =
+let pp_open ?classes tag =
   match classes with
-  | None -> F.fprintf f {|<%s>|} tag
-  | Some classes -> F.fprintf f {|<%s class="%a">|} tag pp_classes classes
+  | None -> F.dprintf {|<%s>|} tag
+  | Some classes -> F.dprintf {|<%s class="%t">|} tag (pp_classes classes)
 
-let pp_close f tag = F.fprintf f "</%s>" tag
+let pp_close tag = F.dprintf "</%s>" tag
+let pp_wrap tag ?classes pp = pp_open ?classes tag ++ pp ++ pp_close tag
+let pp_list_with_line pp l = pp_list ~pp_sep:(pp_print_char '\n') pp l
 
-let pp_wrap tag ?classes pp f x =
-  pp_open ?classes f tag;
-  pp f x;
-  pp_close f tag
-
-let pp_list_with_line f = pp_list ~pp_sep:(fun f -> F.pp_print_char f '\n') f
-
-let rec pp_span f = function
+let rec pp_span = function
   | A s ->
       let short_link =
         if String.starts_with s ~prefix:"https://" then Str.string_after s 8
         else s
       in
-      F.fprintf f {|<a href="%a">%a</a>|} pp_chars s pp_chars short_link
-  | Br -> pp_open f "br"
-  | CharSpan c -> pp_char f c
-  | CharsSpan s -> pp_chars f s
-  | StackOpen Em -> pp_open f "em"
-  | StackClose Em -> pp_close f "em"
-  | StackOpen Strong -> pp_open f "strong"
-  | StackClose Strong -> pp_close f "strong"
-  | StackOpen EmStrong ->
-      pp_open f "em";
-      pp_open f "strong"
-  | StackClose EmStrong ->
-      pp_close f "strong";
-      pp_close f "em"
-  | StackOpen Strike -> pp_open f "s"
-  | StackClose Strike -> pp_close f "s"
-  | StackOpen code -> pp_open f "code"
-  | StackClose code -> pp_close f "code"
-  | UnicodeSpan s -> F.pp_print_string f s
+      F.dprintf {|<a href="%t">%t</a>|} (pp_chars s) (pp_chars short_link)
+  | Br -> pp_open "br"
+  | CharSpan c -> pp_char c
+  | CharsSpan s -> pp_chars s
+  | StackOpen Em -> pp_open "em"
+  | StackClose Em -> pp_close "em"
+  | StackOpen Strong -> pp_open "strong"
+  | StackClose Strong -> pp_close "strong"
+  | StackOpen EmStrong -> pp_open "em" ++ pp_open "strong"
+  | StackClose EmStrong -> pp_close "strong" ++ pp_close "em"
+  | StackOpen Strike -> pp_open "s"
+  | StackClose Strike -> pp_close "s"
+  | StackOpen code -> pp_open "code"
+  | StackClose code -> pp_close "code"
+  | UnicodeSpan s -> pp_print_string s
   | UnsafeA { spans; link } ->
-      F.fprintf f {|<a href="%a">%a</a>|} pp_chars link pp_span_list spans
+      F.dprintf {|<a href="%t">%t</a>|} (pp_chars link) (pp_span_list spans)
 
-and pp_span_list f = pp_list pp_span f
+and pp_span_list l = pp_list pp_span l
 
-let rec pp_block ~rss f = function
+let rec pp_block ~rss = function
   | CodeBlock code_block ->
-      pp_wrap "pre" (pp_wrap "code" (pp_list_with_line pp_chars)) f code_block
+      pp_wrap "pre" (pp_wrap "code" (pp_list_with_line pp_chars code_block))
   | Details { title; body } ->
-      pp_wrap "details"
-        (fun f () ->
-          pp_wrap "summary" pp_span_list f title;
-          (pp ~rss) f body)
-        f ()
-  | H1 sps -> pp_wrap "h1" pp_span_list f sps
-  | H2 sps -> pp_wrap "h2" pp_span_list f sps
-  | H3 sps -> pp_wrap "h3" pp_span_list f sps
-  | H4 sps -> pp_wrap "h4" pp_span_list f sps
-  | H5 sps -> pp_wrap "h5" pp_span_list f sps
-  | H6 sps -> pp_wrap "h6" pp_span_list f sps
-  | Hr -> pp_open f "hr"
-  | List_ (Ordered, lis) -> pp_wrap "ol" (pp_list_with_line (pp_li ~rss)) f lis
-  | List_ (Unordered, lis) ->
-      pp_wrap "ul" (pp_list_with_line (pp_li ~rss)) f lis
-  | P sps -> pp_wrap "p" pp_span_list f sps
-  | Quote quote -> pp_wrap "blockquote" (pp ~rss) f quote
+      pp_wrap "details" (pp_wrap "summary" (pp_span_list title) ++ pp ~rss body)
+  | H1 sps -> pp_wrap "h1" (pp_span_list sps)
+  | H2 sps -> pp_wrap "h2" (pp_span_list sps)
+  | H3 sps -> pp_wrap "h3" (pp_span_list sps)
+  | H4 sps -> pp_wrap "h4" (pp_span_list sps)
+  | H5 sps -> pp_wrap "h5" (pp_span_list sps)
+  | H6 sps -> pp_wrap "h6" (pp_span_list sps)
+  | Hr -> pp_open "hr"
+  | List_ (Ordered, lis) -> pp_wrap "ol" (pp_list_with_line (pp_li ~rss) lis)
+  | List_ (Unordered, lis) -> pp_wrap "ul" (pp_list_with_line (pp_li ~rss) lis)
+  | P sps -> pp_wrap "p" (pp_span_list sps)
+  | Quote quote -> pp_wrap "blockquote" (pp ~rss quote)
   | UnsafeCodeBlock { cb; classes } ->
-      let pp_wrap_code pp f x =
-        let classes = if rss then None else Some classes in
-        pp_wrap "code" ?classes pp f x
-      in
-      pp_wrap "pre" (pp_wrap_code (pp_list_with_line pp_chars)) f cb
+      let classes = if rss then None else Some classes in
+      pp_wrap "pre" (pp_wrap "code" ?classes (pp_list_with_line pp_chars cb))
   | UnsafeImg { alt; link; classes } ->
-      let pp_class_prop f classes =
-        if not rss then F.fprintf f {| class="%a"|} pp_classes classes
+      let pp_class_prop =
+        if not rss then F.dprintf {| class="%t"|} (pp_classes classes)
+        else pp_nop
       in
-      let pp_img f () =
-        F.fprintf f {|<img alt="%a" src="%a"%a>|} pp_chars alt pp_chars link
-          pp_class_prop classes
+      let pp_img =
+        F.dprintf {|<img alt="%t" src="%t"%t>|} (pp_chars alt) (pp_chars link)
+          pp_class_prop
       in
-      pp_wrap "p" pp_img f ()
+      pp_wrap "p" pp_img
   | UnsafeInlineHtml lines ->
-      if not rss then
-        pp_list
-          ~pp_sep:(fun f -> F.pp_print_char f '\n')
-          F.pp_print_string f lines
+      if not rss then pp_list ~pp_sep:(pp_print_char '\n') pp_print_string lines
+      else pp_nop
 
-and pp_li ~rss f = function
-  | Li sps -> pp_wrap "li" pp_span_list f sps
-  | LiP blocks -> pp_wrap "li" (pp ~rss) f blocks
+and pp_li ~rss = function
+  | Li sps -> pp_wrap "li" (pp_span_list sps)
+  | LiP blocks -> pp_wrap "li" (pp ~rss blocks)
 
-and pp ?(rss = false) f = pp_list_with_line (pp_block ~rss) f
+and pp ~rss blocks = pp_list_with_line (pp_block ~rss) blocks
+
+let pp ?(rss = false) f blocks = pp ~rss blocks f
